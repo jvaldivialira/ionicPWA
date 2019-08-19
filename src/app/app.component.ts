f32f66c10b873589e3233a5d278d830294368ace
unknown <joeazor1995@gmail.com>
2019-08-19 18:48:59 -0500

    Primer commit de ionic pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,34 @@
-import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+export class HomePage implements OnInit {
+ 
+  users = [];
+ 
+  constructor(private apiService: ApiService, private plt: Platform) { }
+ 
+  ngOnInit() {
+    this.plt.ready().then(() => {
+      this.loadData(true);
     });
+  }
+ 
+  loadData(refresh = false, refresher?) {
+    this.apiService.getUsers(refresh).subscribe(res => {
+      this.users = res;
+      if (refresher) {
+        refresher.target.complete();
+      }
+    });
+  }
+ 
+  updateUser(id) {
+    this.apiService.updateUser(id, {name: 'Simon', job: 'CEO'}).subscribe();
   }
 }
